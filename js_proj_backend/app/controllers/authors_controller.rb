6c0bf8d722628e4be7768058b83eb60dbb5aa262
HeartyRhombus <kaylahmg@gmail.com>
2021-01-04 13:09:34 -0800

    render database in json

--- js_proj_backend/app/controllers/authors_controller.rb
+++ js_proj_backend/app/controllers/authors_controller.rb
@@ -1,2 +1,7 @@
 class AuthorsController < ApplicationController
+
+    def index
+        authors = Author.all
+        render json: authors, only: [:id, :first_name, :last_name]
+    end
 end
--- js_proj_backend/app/controllers/books_controller.rb
+++ js_proj_backend/app/controllers/books_controller.rb
@@ -1,2 +1,8 @@
 class BooksController < ApplicationController
+
+    def index
+        books = Book.all
+        render json: books, only: [:id, :title, :genre, :pub_date, :summary], include: [:author]
+    end
+
 end

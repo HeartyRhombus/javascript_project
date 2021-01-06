191dc6fbed3490497f9c99ada08e66e58033c318
HeartyRhombus <kaylahmg@gmail.com>
2021-01-06 07:53:40 -0800

    add create route to books controller

--- js_proj_backend/app/controllers/books_controller.rb
+++ js_proj_backend/app/controllers/books_controller.rb
@@ -10,4 +10,24 @@ class BooksController < ApplicationController
         render json: book
     end
 
+    def create
+        @book = Book.new(book_params)
+    
+        if @book.save
+          render json: @book, status: :created, location: @book
+        else
+          render json: @book.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_book
+        @book = Book.find(params[:id])
+      end
+  
+      # Only allow a trusted parameter "white list" through.
+      def book_params
+        params.require(:book).permit(:title, :author_id, :genre, :pub_date, :summary)
+      end
 end

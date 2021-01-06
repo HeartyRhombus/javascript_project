7a8f73ecde2b3bfb781c0b4fe049e91cb42bf4b8
HeartyRhombus <kaylahmg@gmail.com>
2021-01-06 08:00:51 -0800

    define set_author and author_params methods

--- js_proj_backend/app/controllers/authors_controller.rb
+++ js_proj_backend/app/controllers/authors_controller.rb
@@ -9,4 +9,15 @@ class AuthorsController < ApplicationController
         author = Author.find_by(id: params[:id])
         render json: author
     end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_author
+      @author = Author.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def author_params
+      params.require(:author).permit(:first_name, :last_name)
+    end
 end

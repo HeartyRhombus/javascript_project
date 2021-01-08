2808c7a02ee08d52fb3bdc1210d97d7d13f160e8
HeartyRhombus <kaylahmg@gmail.com>
2021-01-08 14:31:28 -0800

    restrict to used routes

--- js_proj_backend/config/routes.rb
+++ js_proj_backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :authors
+  resources :authors, only: [:index, :create]
   resources :books do
-    resources :authors
+    resources :authors, only: [:index, :create]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

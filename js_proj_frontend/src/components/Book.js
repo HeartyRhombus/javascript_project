fabaa3309fcfc561ea261aab8948e7bbfb3d2995
HeartyRhombus <kaylahmg@gmail.com>
2021-01-08 08:02:20 -0800

    create book class & constructor

---
+++ js_proj_frontend/src/components/Book.js
@@ -0,0 +1,11 @@
+class Book{
+
+    constructor(data){
+        this.id = data.id
+        this.title = data.title
+        this.genre = data.genre
+        this.pub_date = data.pub_date
+        this.author = data.author
+    }
+
+}

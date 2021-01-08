0d5305fe58a45f402210a1641cc35ce31ff16380
HeartyRhombus <kaylahmg@gmail.com>
2021-01-08 14:31:03 -0800

    remove unused actions

--- js_proj_backend/app/controllers/authors_controller.rb
+++ js_proj_backend/app/controllers/authors_controller.rb
@@ -1,14 +1,8 @@
 class AuthorsController < ApplicationController
-    before_action :set_author, only: [:show, :update, :destroy]
 
     def index
         authors = Author.all
         render json: authors
-    end
-
-    def show
-        author = Author.find_by(id: params[:id])
-        render json: author
     end
 
       # POST /authors
@@ -22,26 +16,7 @@ class AuthorsController < ApplicationController
         end
     end
 
-      # PATCH/PUT /authors/1
-    def update
-        if @author.update(author_params)
-        render json: @author
-        else
-        render json: @author.errors, status: :unprocessable_entity
-        end
-    end
-
-    # DELETE /authors/1
-    def destroy
-        @author.destroy
-    end
-
     private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_author
-      @author = Author.find(params[:id])
-    end
-
     # Only allow a trusted parameter "white list" through.
     def author_params
       params.require(:author).permit(:first_name, :last_name)

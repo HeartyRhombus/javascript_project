d6e2eb9ec82ee20e4386d5dc1b81e6a2fa1c01b0
HeartyRhombus <kaylahmg@gmail.com>
2021-01-08 09:22:28 -0800

    add displayBook method & add summary to constructor

--- js_proj_frontend/src/components/Book.js
+++ js_proj_frontend/src/components/Book.js
@@ -5,6 +5,7 @@ class Book{
         this.title = data.title
         this.genre = data.genre
         this.pub_date = data.pub_date
+        this.summary = data.summary
         this.author = data.author
     }
 
@@ -13,6 +14,25 @@ class Book{
         <li>
             <a href="#" data-id="${this.id}">${this.title}</a>
         </li>
+        `
+    }
+
+    displayBook(book){
+        return `
+        <h3>${this.title}</h3>
+        By: ${this.author.first_name} ${this.author.last_name}
+        <hr>
+        Publication Date: ${this.pub_date}
+        <br/>
+        Genre: ${this.genre}
+        <br/>
+        Summary:
+        <br/>
+        ${this.summary}
+        <br/>
+        <hr>
+        <button id="edit_book" data-id="${this.id}">Edit</button>
+        <button id="delete_book" data-id="${this.id}">Delete</button>
         `
     }
 

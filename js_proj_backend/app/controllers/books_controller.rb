401929f88a2b3300e1a71639e4cd7f7722f8ecda
HeartyRhombus <kaylahmg@gmail.com>
2021-04-13 14:41:47 -0700

    remove unneeded '@'

--- js_proj_backend/app/controllers/books_controller.rb
+++ js_proj_backend/app/controllers/books_controller.rb
@@ -3,8 +3,8 @@ class BooksController < ApplicationController
 
     # GET /books
     def index
-        @books = Book.all
-        render json: @books
+        books = Book.all
+        render json: books
     end
 
     # GET /books/1

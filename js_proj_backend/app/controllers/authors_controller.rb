52af724d16678912d030d88b2c6ad066d5b0784d
HeartyRhombus <kaylahmg@gmail.com>
2021-04-19 16:20:18 -0700

    add show route & before_action in controller

--- js_proj_backend/app/controllers/authors_controller.rb
+++ js_proj_backend/app/controllers/authors_controller.rb
@@ -1,8 +1,14 @@
 class AuthorsController < ApplicationController
+  before_action :set_author, only: [:show]
 
     def index
-        authors = Author.all
-        render json: authors
+        @authors = Author.all
+        render json: @authors
+    end
+
+    def show
+      # binding.pry
+      render json: @author
     end
 
       # POST /authors
@@ -17,6 +23,12 @@ class AuthorsController < ApplicationController
     end
 
     private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_author
+      @author = Author.find(params[:id])
+    end
+
     # Only allow a trusted parameter "white list" through.
     def author_params
       params.require(:author).permit(:first_name, :last_name)
--- js_proj_backend/config/routes.rb
+++ js_proj_backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :authors, only: [:index, :create]
+  resources :authors, only: [:index, :show, :create]
   resources :books do
-    resources :authors, only: [:index, :create]
+    resources :authors, only: [:index, :show, :create]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
